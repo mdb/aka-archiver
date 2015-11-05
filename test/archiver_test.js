7c68a53063276d0ed10a000ea8e8d548e62f1cc1
Mike Ball <mikedball@gmail.com>
2015-11-05 12:16:09 -0500

    add some basic archiver tests

---
+++ test/archiver_test.js
@@ -0,0 +1,66 @@
+var Archiver = require('../archiver'),
+    archiver = new Archiver({
+      clientToken: 'clientToken',
+      clientSecret: 'clientSecret',
+      accessToken: 'accessToken',
+      edgegridHost: 'https://host.com'
+    }),
+    fs = require('fs'),
+    assert = require('assert'),
+    request = require('supertest'),
+    nock = require('nock'),
+    mockGet = function(path, resp) {
+      return nock('https://host.com')
+        .get(path)
+        .reply(200, resp);
+    };
+
+describe('archiver', function() {
+  describe('#domain', function() {
+    it('performs a request to the proper domain endpoint and archives its response', function(done) {
+      mockGet('/config-gtm/v1/domains/someDomain', 'domains');
+
+      archiver.domain('someDomain', function(err) {
+        assert.equal(fs.readFileSync('someDomain_domain.json'), 'domains');
+
+        done();
+      });
+    });
+  });
+
+  describe('#properties', function() {
+    it('performs a request to the proper properties endpoint and archives its response', function(done) {
+      mockGet('/config-gtm/v1/domains/someDomain/properties', 'properties');
+
+      archiver.properties('someDomain', function(err) {
+        assert.equal(fs.readFileSync('someDomain_properties.json'), 'properties');
+
+        done();
+      });
+    });
+  });
+
+  describe('#dataCenters', function() {
+    it('performs a request to the proper data centers endpoint and archives its response', function(done) {
+      mockGet('/config-gtm/v1/domains/someDomain/datacenters', 'dataCenters');
+
+      archiver.dataCenters('someDomain', function(err) {
+        assert.equal(fs.readFileSync('someDomain_dataCenters.json'), 'dataCenters');
+
+        done();
+      });
+    });
+  });
+
+  describe('#datacenters', function() {
+    it('performs a request to the proper data centers endpoint and archives its response', function(done) {
+      mockGet('/config-gtm/v1/domains/someDomain/datacenters', 'dataCenters');
+
+      archiver.datacenters('someDomain', function(err) {
+        assert.equal(fs.readFileSync('someDomain_dataCenters.json'), 'dataCenters');
+
+        done();
+      });
+    });
+  });
+});
